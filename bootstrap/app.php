9b6987cdc8b0d1cf6a27c7a8a47fb7e19e959831
Daniel <hola@danielcortes.xyz>
2026-03-28 00:56:05 -0300

    agregando el trust proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        if (app()->environment('production')) {
+            $middleware->trustProxies(
+                ['private_ranges'],
+                Request::HEADER_X_FORWARDED_TRAEFIK,
+            );
+        }
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
